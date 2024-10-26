8d9cb119024c91d8a2313d3ed262e430e2bf6cb1
UnCompa <brandonddxd@gmail.com>
2024-10-26 15:46:46 -0500

    Push all files for the project

--- src/main/java/com/krakedev/moduloii/evaluacionfinal/utils/ConexionBDD.java
+++ src/main/java/com/krakedev/moduloii/evaluacionfinal/utils/ConexionBDD.java
@@ -1,38 +1,30 @@
 package com.krakedev.moduloii.evaluacionfinal.utils;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
 
 import com.krakedev.moduloii.evaluacionfinal.excepciones.InventarioException;
 
 
 public class ConexionBDD {
-	private final static String DRIVER = "org.postgresql.Driver";
-	private final static String URL = "jdbc:postgresql://localhost:5432/ INVENTARIOS_KRAKEDEV";
-	private final static String USER = "postgres";
-	private final static String PASSWORD = "Amigo 12345";
-	private static Logger logger = LogManager.getLogger(ConexionBDD.class);
-	
 	public static Connection conectar() throws InventarioException{
-		Connection connection = null;
-
+		Context ctx = null;
+		DataSource ds = null;
+		Connection con = null;
 		try {
-			Class.forName(DRIVER);
-			connection = DriverManager.getConnection(URL, USER, PASSWORD);
-		} catch (ClassNotFoundException e) {
+			ctx = new InitialContext();
+			ds = (DataSource) ctx.lookup("java:/comp/env/jdbc/PgDB");
+			con = ds.getConnection();
+		} catch (NamingException | SQLException e) {
 			e.printStackTrace();
-			logger.error("error en la infraestrucutra",e);
-			throw new InventarioException("Error en la infraestrucutra");
-		} catch (SQLException e) {
-			e.printStackTrace();
-			logger.error("Error al conectarse",e);
 			throw new InventarioException("Error al conectarse");
 		}
-		return connection;
+		return con;
 	}
 }
 
