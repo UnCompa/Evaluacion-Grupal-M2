6f1d1f340ff5c3ca66f67f1aa8197672eb5431ae
UnCompa <brandonddxd@gmail.com>
2024-10-26 17:40:20 -0500

    feat: :sparkles: Cambios pequeños

--- src/main/java/com/krakedev/moduloii/evaluacionfinal/servicios/ServiciosGrupo.java
+++ src/main/java/com/krakedev/moduloii/evaluacionfinal/servicios/ServiciosGrupo.java
@@ -1,6 +1,8 @@
 package com.krakedev.moduloii.evaluacionfinal.servicios;
 
+import javax.websocket.server.PathParam;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.MediaType;
@@ -10,7 +12,7 @@ import com.krakedev.moduloii.evaluacionfinal.entidades.Grupo;
 import com.krakedev.moduloii.evaluacionfinal.excepciones.InventarioException;
 import com.krakedev.moduloii.evaluacionfinal.persistencia.GrupoBDD;
 
-@Path ("grupos")
+@Path("grupos")
 public class ServiciosGrupo {
 
 	public void insertar(Grupo grupo) {
@@ -32,7 +34,21 @@ public class ServiciosGrupo {
 		GrupoBDD nuevoGrupo= new GrupoBDD();
 		try {
 			nuevoGrupo.agregarGrupo(grupo);
-			return Response.ok("NUEVO GRUPO").build();
+			return Response.ok("GRUPO CREADO").build();
+		} catch (InventarioException e) {
+			return Response.serverError().build();
+			
+		}
+	
+	}
+	@Path("borrar/{id}")
+	@DELETE()
+	public Response borrarGrupo(@PathParam("id") String id) {
+		GrupoBDD nuevoGrupo= new GrupoBDD();
+		System.out.println(id);
+		try {
+			nuevoGrupo.borrarGrupo(id);
+			return Response.ok("GRUPO BORRADO").build();
 		} catch (InventarioException e) {
 			return Response.serverError().build();
 			
